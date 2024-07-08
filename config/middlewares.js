b2aa2054b4e8258762d1ddce9d0e49151ec9624b
shadan-asad <iamshadanasad@gmail.com>
2024-07-09 01:03:46 +0530

    Updated the cors settings

--- config/middlewares.js
+++ config/middlewares.js
@@ -2,7 +2,13 @@ module.exports = [
   "strapi::logger",
   "strapi::errors",
   "strapi::security",
-  "strapi::cors",
+  {
+    name: "strapi::cors",
+    config: {
+      enabled: true,
+      origin: ["http://localhost:4200", "https://ichat-three.vercel.app"],
+    },
+  },
   "strapi::poweredBy",
   "strapi::query",
   "strapi::body",
